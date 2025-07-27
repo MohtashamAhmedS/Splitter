c6f128aba4fa544abefe70b5bfd1cbc7ff003116
MohtashamAhmedS <mohtashamahmed2005@gmail.com>
2025-07-27 18:23:36 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <h1 className="logo-heading">Splitter.</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
